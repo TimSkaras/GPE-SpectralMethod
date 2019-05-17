5758af18a2d57e4b1659b1fa4a44c319d1d2e078
Tim Skaras <tims5847@gmail.com>
2019-05-17 13:55:59 -0500

    Implemented GPU code with near order of magnitude speed up

--- TimeSplittingSpectral-GPU.py
+++ TimeSplittingSpectral-GPU.py
@@ -27,6 +27,14 @@ def getNorm(waveFunction):
 
     return np.sqrt(np.sum(waveFunction*hz))
 
+def reduceIntegrateCUDA(waveFunction):
+    """
+    This function takes a three dimensional array of values and redcues it to a
+    single dimensional array by integrating over the x and y dimensions
+    """
+    
+    return cp.sum(cp.abs(waveFunction)**2, axis=(0,1))*hx*hy
+
 def reduceIntegrate(waveFunction):
     """
     This function takes a three dimensional array of values and redcues it to a
@@ -35,46 +43,42 @@ def reduceIntegrate(waveFunction):
     
     return np.sum(np.abs(waveFunction)**2, axis=(0,1))*hx*hy
     
-def realTimePropagation(psi_initial):
+def realTimePropagation(solution):
     """
     This function numerically solves the GPE in real time using the spectral method
     
     """
-    
-    solution = np.copy(psi_initial)
-    psiPlot = np.array([reduceIntegrate(psi_initial)]) # Index Convention: psiPlot[time idx][space idx]
-    mux = 2*np.pi/LX * np.arange(-NX/2, NX/2)
-    muy = 2*np.pi/LY * np.arange(-NY/2, NY/2)
-    muz = 2*np.pi/LZ * np.arange(-NZ/2, NZ/2)
-    muExpTerm = np.einsum('i,j,k->ijk', np.exp(-1j*dt*mux**2/2.), np.exp(-1j*dt*muy**2/2.), np.exp(-1j*dt*muz**2/2.))
-    muExpTerm = np.fft.ifftshift(muExpTerm)
+#    np_solution = cp.asnumpy(solution)
+    psiPlot = np.array([cp.asnumpy(reduceIntegrateCUDA(solution))]) # Index Convention: psiPlot[time idx][space idx]
+    mux = 2*cp.pi/LX * cp.arange(-NX/2, NX/2)
+    muy = 2*cp.pi/LY * cp.arange(-NY/2, NY/2)
+    muz = 2*cp.pi/LZ * cp.arange(-NZ/2, NZ/2)
+    muExpTerm = cp.einsum('i,j,k->ijk', cp.exp(-1j*dt*mux**2/2.), cp.exp(-1j*dt*muy**2/2.), cp.exp(-1j*dt*muz**2/2.))
+    muExpTerm = cp.fft.ifftshift(muExpTerm)
         
-    potential = np.einsum('i,j,k->ijk', np.exp(0.5*WX**2*(x - (xa+xb)/2.)**2), np.exp(0.5*WY**2*(y - (ya+yb)/2.)**2), 
-                              np.exp(0.5*WZ**2*(z-(za+zb)/2.)**2))
-    potential = np.log(potential) 
-    
-    fourierCoeffsManual = np.zeros((NX, NY, NZ), dtype=np.complex_)
-    invFourier = np.zeros((NX, NY, NZ), dtype=np.complex_)
+    potential = cp.einsum('i,j,k->ijk', cp.exp(0.5*WX**2*(x - (xa+xb)/2.)**2), cp.exp(0.5*WY**2*(y - (ya+yb)/2.)**2), 
+                              cp.exp(0.5*WZ**2*(z-(za+zb)/2.)**2))
+    potential = cp.log(potential) 
     
     for p in range(TIME_PTS):
         
         # Step One -- potential and interaction term
-        test = np.copy(solution)
-        expTerm = np.exp(-1j* (potential + G*np.abs(test)**2)*dt/2.0)
+        expTerm = cp.exp(-1j* (potential + G*cp.abs(solution)**2)*dt/2.0)
         solution = expTerm*solution
                     
                     
         # Step Two -- kinetic term
-        fourierCoeffs = np.fft.fftn(solution)
+        fourierCoeffs = cp.fft.fftn(solution)
         fourierCoeffs = fourierCoeffs*muExpTerm
-        solution = np.fft.ifftn(fourierCoeffs)
+        solution = cp.fft.ifftn(fourierCoeffs)
         
         # Step Three -- potential and interaction term again
-        expTerm = np.exp(-1j* (potential + G*np.abs(solution)**2)*dt/2.0)
+        expTerm = cp.exp(-1j* (potential + G*cp.abs(solution)**2)*dt/2.0)
         solution = expTerm*solution
         
         # Save Solution for plotting
-        psiPlot = np.vstack((psiPlot, reduceIntegrate(solution))) 
+#        np_solution = cp.asnumpy(solution)
+        psiPlot = np.vstack((psiPlot, cp.asnumpy(reduceIntegrateCUDA(solution)))) 
         
     return psiPlot
     
@@ -83,10 +87,10 @@ def realTimePropagation(psi_initial):
 # ----------- Important variables --------------
 
 # Spatial Points
-N = 80
+N = 32
 NX = N
 NY = N
-NZ = N
+NZ = 512
 
 # Trap length
 L = 8.0
@@ -109,7 +113,7 @@ hx = LX/NX
 hy = LY/NY
 hz = LZ/NZ
 
-TIME_PTS = 50
+TIME_PTS = 100
 
 dt = TIME/TIME_PTS
 G = 10. # interaction strength parameter
@@ -120,28 +124,28 @@ WZ = W
 A = .30 + .40j # Coefficient for homogeneous solution
 
 # Establish grid points
-x = hx*np.arange(NX) + xa
-y = hy*np.arange(NY) + ya
-z = hz*np.arange(NZ) + za
+x = hx*cp.arange(NX) + xa
+y = hy*cp.arange(NY) + ya
+z = hz*cp.arange(NZ) + za
 
 # ------------- Initial Conditions -------------
 
 ## Soliton solution
 
 ## Homogeneous solution
-#psi_init = A*np.ones((NX, NY, NZ))
+#psi_init = A*cp.ones((NX, NY, NZ))
 
 # Gaussian initial
 sigma = 1.0
-psi_init = 1/np.sqrt(2*np.pi) * np.einsum('i,j,k->ijk', np.exp(-x**2/(2*sigma**2)), np.exp(-y**2/(2*sigma**2)), np.exp(-z**2/(2*sigma**2)))
+psi_init = 1/cp.sqrt(2*cp.pi) * cp.einsum('i,j,k->ijk', cp.exp(-x**2/(2*sigma**2)), cp.exp(-y**2/(2*sigma**2)), cp.exp(-z**2/(2*sigma**2)))
 
 ## Plane Wave
-#mu = 2*np.pi*np.array([0/LX, 1/LY, -2/LZ])
-#psi_init = np.ones((NX, NY, NZ), dtype=np.complex_)
-#for i in np.arange(NX):
-#    for j in np.arange(NY):
-#        for k in np.arange(NZ):
-#            psi_init[i,j,k] = np.exp(1j*(mu[0]*x[i] + mu[1]*y[j] + mu[2]*z[k]))
+#mu = 2*cp.pi*cp.array([0/LX, 1/LY, -2/LZ])
+#psi_init = cp.ones((NX, NY, NZ), dtype=cp.complex_)
+#for i in cp.arange(NX):
+#    for j in cp.arange(NY):
+#        for k in cp.arange(NZ):
+#            psi_init[i,j,k] = cp.exp(1j*(mu[0]*x[i] + mu[1]*y[j] + mu[2]*z[k]))
 #            
 
 begin = timeit.default_timer()
@@ -154,9 +158,11 @@ print(f'Grid Points = {NX*NY*NZ}')
 
 ## Norm conservation as function of time
 norm_time = [getNorm(psiPlot[j, :]) for j in range(len(psiPlot))]
-print(f'Norm Ratio = {norm_time[-1]/norm_time[0] - 1}')
+print(f'Norm Ratio = {norm_time[-1]/norm_time[0] - 1}\n')
 
-#homExact = A*np.exp(-1j*G*np.abs(A)**2 * TIME)
+print(f'Estimated Runtime = {(end-begin)/TIME_PTS * 32000 / 60} min')
+
+#homExact = A*cp.exp(-1j*G*cp.abs(A)**2 * TIME)
 #print('Homogeneous Solution = ' + f'{homExact}')
 
 #fig = plt.figure(1)   # Clear figure 2 window and bring forward
@@ -165,8 +171,8 @@ print(f'Norm Ratio = {norm_time[-1]/norm_time[0] - 1}')
 #plt.ylabel("Norm of Wave Function")
 #plt.xlabel('Time')
 
-
-xx,tt = np.meshgrid(z,np.arange(TIME_PTS+1)*dt)
+z_np = hz*np.arange(NZ) + za
+xx,tt = np.meshgrid(z_np,np.arange(TIME_PTS+1)*dt)
 fig = plt.figure(2)   # Clear figure 2 window and bring forward
 ax = fig.gca(projection='3d')
 surf = ax.plot_surface(xx, tt, psiPlot, rstride=1, cstride=1, cmap=cm.jet,linewidth=0, antialiased=False)
